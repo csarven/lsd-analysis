02999ec63e6007cb58f5bf1775c9469fb4a24246
Sarven Capadisli <info@csarven.ca>
2013-11-10 19:55:05 +0100

    Some helper scripts

---
+++ shiny.install.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo npm install -g https://github.com/rstudio/shiny-server/tarball/master
---
+++ shiny.post-install.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sed -i 's/__assets__/\/__assets__/' /usr/lib/node_modules/shiny-server/R/SockJSAdapter.R
