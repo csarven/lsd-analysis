4f84eade52de7c635dafb802b44297694240922a
Sarven Capadisli <info@csarven.ca>
2014-02-06 11:41:07 +0100

    Lets Shiny server know the service location

--- shiny.post-install.sh
+++ shiny.post-install.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 sed -i 's/__assets__/\/__assets__/' /usr/lib/node_modules/shiny-server/R/SockJSAdapter.R
+
+sudo mkdir -p /etc/shiny-server/
+sudo cp server-config.conf /etc/shiny-server/
