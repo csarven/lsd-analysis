e029923ba0d5dcca87ba4339c0103a6cf88b2d2d
Sarven Capadisli <info@csarven.ca>
2014-02-12 20:57:16 +0100

    Using symlinks

--- shiny.post-install.sh
+++ shiny.post-install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-sed -i 's/__assets__/\/__assets__/' /usr/lib/node_modules/shiny-server/R/SockJSAdapter.R
+sed -i 's/'\''__assets__/'\''\/__assets__/g' /usr/lib/node_modules/shiny-server/R/SockJSAdapter.R
+
+ln -s /usr/local/lib/R/site-library/shiny/www/shared/shiny.js www/shiny/shiny.js
+ln -s /usr/local/lib/R/site-library/shiny/www/shared/jquery.js www/shiny/jquery.js
+
 
 sudo mkdir -p /etc/shiny-server/
-sudo cp server-config.conf /etc/shiny-server/
+sudo ln -s /var/shiny-server/www/lsd-analysis/shiny-server.conf /etc/shiny-server/shiny-server.conf
